5dad1e7b7020cfbfbe60253ded2ae9092d26df68
DyingRusher <arjavankoliya@gmail.com>
2024-01-17 12:21:55 +0530

    Tracking done

--- yolo_with_webcam.py
+++ yolo_with_webcam.py
@@ -2,12 +2,18 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+from sort import * #it keep track of objects bypixel matching every frame if conf is good it give same id to it
+                    #for working, it required np array with [x1,y1,x2,y2,conf] for every itration
+
+
 
 cap = cv2.VideoCapture('C:/Users/arjav/OneDrive/Desktop/object_detection (1)/car_counter/video.mp4')
 # cap.set(3,640)
 # cap.set(4,480)
 mask = cv2.imread('C:/Users/arjav/OneDrive/Desktop/object_detection (1)/car_counter/mask.png')
+
 model = YOLO('C:/Users/arjav/OneDrive/Desktop/object_detection (1)/yolov8n.pt')
+
 yolo_map = {0: 'person',
  1: 'bicycle',
  2: 'car',
@@ -89,6 +95,9 @@ yolo_map = {0: 'person',
  78: 'hair drier',
  79: 'toothbrush'}
 
+#tracking 
+tracker = Sort(max_age=20,min_hits=2,iou_threshold=0.3)
+
 while True:
     
     suc ,img = cap.read()
@@ -96,6 +105,7 @@ while True:
     res = model(imgreg,stream = True)
     # print(res)
     
+    detection = np.empty((0,5))
     for r in res:
         boxes = r.boxes
         for b in boxes:
@@ -110,12 +120,22 @@ while True:
             conf = (math.ceil(b.conf*100))/100 #0.343553 to 0.34
             cla = int(b.cls[0])
             # print(yolo_map[cla])
-            if(yolo_map[cla] == 'truck' or yolo_map[cla] == 'bus' or yolo_map[cla] == 'motorcycle' or yolo_map[cla] == 'car'):
-                cvzone.putTextRect(img,f'{conf} {yolo_map[cla]}',(max(0,x1),max(30,y1)),scale=1,thickness=1,offset=1)
-                cvzone.cornerRect(img,bbox,l=3)
+            if(yolo_map[cla] == 'truck' or yolo_map[cla] == 'bus' or yolo_map[cla] == 'motorcycle' or yolo_map[cla] == 'car' and conf>0.3):
+                # cvzone.putTextRect(img,f'{conf} {yolo_map[cla]}',(max(0,x1),max(30,y1)),scale=1,thickness=1,offset=1)
+                # cvzone.cornerRect(img,bbox,l=3)
+                currentArray = np.array([x1,y1,x2,y2,conf])
+                detection = np.vstack([detection,currentArray])
             # cv2.putText(img,str(conf),(int(x1),int(y2-10)),cv2.FONT_HERSHEY_SIMPLEX,1,(25,25,134),2,cv2.LINE_AA)
-            
+    
+    resultTracker = tracker.update(detection)
+    for res in resultTracker:
+        x1,y1,x2,y2,id = res
+        x1,y1,x2,y2  = int(x1),int(y1),int(x2),int(y2)
+        cvzone.putTextRect(img,f'{id}',(max(0,x1),max(30,y1)),scale=1,thickness=1,offset=1)
+        cvzone.cornerRect(img,(x1,y1,x2-x1,y2-y1),l=3)
+        
     cv2.imshow("sds",img)
     intr = cv2.waitKey(0)
-    if intr & 0xFF == ord('q') :
+    
+    if intr & 0xFF == ord('q'):
         break
